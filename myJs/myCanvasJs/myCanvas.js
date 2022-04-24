7001a8b2ae23c3f84c016c9992384532b9c7bdc0
zhumengren <franciszmr@foxmail.com>
2022-04-24 20:16:41 +0800

    canvas学习更新

---
+++ myJs/myCanvasJs/myCanvas.js
@@ -0,0 +1,6 @@
+// var canvas = document.getElementById("myCanvas01")
+// // 获取画布的2D渲染上下文
+// var ctx = canvas.getContext("2d")
+//
+// // 1、清除一部分画布
+// ctx.clearRect(10, 10, canvas.width, canvas.height)
